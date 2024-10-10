21f40e4395915ebd238266e0e6f6738c33584380
Lifailon <deadepicboss@gmail.com>
2024-10-10 15:16:39 +0300

    add clipboard and check min characters

--- src/Wox.Plugin.LanguageTool/main.py
+++ src/Wox.Plugin.LanguageTool/main.py
@@ -1,9 +1,16 @@
 import requests
+import subprocess
 from wox import Wox
 
 def check_grammar(text):
     results = []
-
+    if len(text.strip()) < 5:
+        results.append({
+            "Title": "Enter at least 5 characters for check grammar",
+            "SubTitle": "",
+            "IcoPath": "logo.png"
+        })
+        return results
     if text:
         url_api = 'https://api.languagetoolplus.com/v2/check'
         headers = {
@@ -15,14 +22,11 @@ def check_grammar(text):
             'language': 'auto',
             'enabledOnly': 'false'
         }
-
         response = requests.post(url_api, headers=headers, data=data)
         data = response.json()
-
         if response.status_code == 200:
             if 'matches' in data and data['matches']:
                 matches = data['matches']
-
                 for match in matches:
                     replacements = [replacement['value'] for replacement in match['replacements']]
                     if replacements:
@@ -32,7 +36,12 @@ def check_grammar(text):
                             results.append({
                                 "Title": replacement,
                                 "SubTitle": f'Source word: {error_word}',
-                                "IcoPath": "logo.png"
+                                "IcoPath": "logo.png",
+                                "JsonRPCAction": {
+                                    "method": "copyToClipboard",
+                                    "parameters": [replacement],
+                                    "dontHideAfterAction": True
+                                }
                             })
             else:
                 results.append({
@@ -52,6 +61,10 @@ def check_grammar(text):
 class GrammarCheck(Wox):
     def query(self, query):
         return check_grammar(query)
+
+    def copyToClipboard(self, text):
+        process = subprocess.Popen('clip', stdin=subprocess.PIPE, shell=True)
+        process.communicate(input=text.encode('utf-16'))
 
 if __name__ == "__main__":
     GrammarCheck()
